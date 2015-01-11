f64e88bc8d888a29efa7eff9995cdc2b3462f1c3
Carolynn Vu <carolynn.vu@gmail.com>
2015-01-11 00:10:09 -0500

    Completed 000.setup

--- 000.setup/src/main/scala/scalax/HelloWorld.scala
+++ 000.setup/src/main/scala/scalax/HelloWorld.scala
@@ -1,5 +1,10 @@
 package scalax
 
 object HelloWorld {
-  def main: Unit = {}
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
 }
+
+//Scala doesn't have static methods; instead it has singletons (here, HelloWorld)
+//Method with Unit return type is like a Java method with a void return. 
