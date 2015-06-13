48fed03e0722bef41cce1a060026d38d6939228a
Carolynn Vu <carolynn.vu@gmail.com>
2015-06-13 19:44:59 -0400

    140.case_classes - Case classes vs classes

--- 140.case_classes/src/main/scala/scalax/Person.scala
+++ 140.case_classes/src/main/scala/scalax/Person.scala
@@ -1,18 +1,18 @@
 package scalax
 
-// define a class CPerson
+class CPerson(_firstName: String, _lastName: String, _age: Int)
 
-// define a case class CCPerson
+case class CCPerson(firstname: String, lastname: String, age: Int)
 
 object Person extends App {
-/*
+
   val cKid = new CPerson("John", "Smith", 9)
   val cAdult = new CPerson("Martin", "Smith", 47)
   val cAdult2 = new CPerson("Martin", "Smith", 47)
 
   println(cKid)
   println(cAdult)
-  println(cAdult == cAdult2)
+  println(cAdult == cAdult2) // == checks for reference equality for ordinary classes
 
   val ccKid = CCPerson("John", "Smith", 9)
   val ccAdult = CCPerson("Martin", "Smith", 47)
@@ -20,6 +20,6 @@ object Person extends App {
 
   println(ccKid)
   println(ccAdult)
-  println(ccAdult == ccAdult2)
-*/
+  println(ccAdult == ccAdult2) // == checks for value equality for case classes
+
 }
